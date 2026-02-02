4a18ae74a30b396759e2bff063f944a49db3ec25
Cickks <michael.painter@selu.edu>
2026-02-02 11:45:03 -0600

    DTO
    location DTO

---
+++ Selu383.SP26.Api/Dtos/LocationDto.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Selu383.SP26.Api.Dtos;
+
+public class LocationDto
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(120)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(200)]
+    public string Address { get; set; } = string.Empty;
+
+    [Range(1, int.MaxValue)]
+    public int TableCount { get; set; }
+}
